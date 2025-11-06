4d2a00cb47674bec183c0651efd44ab18b3688de
Shoaib Shamrez <102520664+shoaibch05@users.noreply.github.com>
2025-11-06 22:17:39 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,6 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306, // ✅ include this
 });
 
 db.connect((err) => {
